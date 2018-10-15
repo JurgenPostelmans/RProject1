5dac6dfe1e3c6eb45f9ef559435f2b3c2fb51930
Jurgen Postelmans <jurgen.postelmans@icloud.com>
2018-10-15 13:47:28 +0200

    Commit

---
+++ SQLServer/3. Lab.R
@@ -0,0 +1,27 @@
+﻿#install.packages("rpart.plot")
+library("RODBC")
+library("rpart")
+library("rpart.plot")
+
+dbconnection <- odbcDriverConnect("Driver={ODBC Driver 13 for SQL Server};Server=springfield;Database=AdventureWorksdw2016;trusted_connection=yes")
+inputData <- sqlQuery(dbconnection, query = "select BikeBuyer, YearlyIncome, NumberCarsOwned, TotalChildren from dbo.vTargetMail")
+
+head(inputData)
+
+tree <- rpart(BikeBuyer ~ YearlyIncome + NumberCarsOwned + TotalChildren, data = inputData, method = "class")
+
+rpart.plot(tree)
+
+newCustomer <- data.frame(YearlyIncome = c(90000), NumberCarsOwned = c(1), TotalChildren = c(1))
+newCustomer$BikeBuyer = predict(tree, newdata = newCustomer,type="class")
+newCustomer$Probability = predict(tree, newdata = newCustomer, type = "prob")
+
+head(newCustomer)
+
+#x is a vector or an integer
+sample(x = 150, size = 100)
+
+#inputData <- sqlQuery(dbconnection, query = "select BikeBuyer, YearlyIncome, NumberCarsOwned, TotalChildren,Region from dbo.vTargetMail")
+#subset(inputData, subset = Region == "Europe")
+
+prp(tree)
---
+++ SQLServer/4. XDF.R
@@ -0,0 +1,17 @@
+﻿list.files(rxGetOption("sampleDataDir"))
+
+mortgagefiles <- list.files(rxGetOption("sampleDataDir"),
+            pattern = "mortDefaultSmall\\d*.csv",
+            full.names = TRUE)
+mortgXdf <- tempfile(fileext = ".xdf")
+
+lapply(mortgagefiles, FUN = function(csv_file) {
+    rxImport(inData = csv_file,
+                outFile = mortgXdf,
+                append = file.exists(mortgXdf))
+    }
+)
+
+rxGetInfo(mortgXdf)
+
+rxGetVarInfo(mortgXdf)
